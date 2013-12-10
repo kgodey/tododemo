0a5285038cf340874ccff78f25bda5fb351abd9f
Kriti Godey <kriti@kritigodey.com>
2013-12-10 17:52:37 -0500

    modularised URLs properly

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,15 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 from todo import views
+from tastypie.api import Api
+from todo.api import ToDoItemResource
+
+v1_api = Api(api_name='v1')
+v1_api.register(ToDoItemResource())
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
+	url(r'^api/', include(v1_api.urls)),
+	url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'todo/login.html'}, name='login'),
+	url(r'^logout/', 'django.contrib.auth.views.logout_then_login', name='logout'),
+	url(r'^signup/', views.signup, name='signup'),
 )
--- todoproject/urls.py
+++ todoproject/urls.py
@@ -1,19 +1,9 @@
 from django.conf.urls import patterns, include, url
-from tastypie.api import Api
-from todo.api import ToDoItemResource
-from todo import views
 
 from django.contrib import admin
 admin.autodiscover()
 
-v1_api = Api(api_name='v1')
-v1_api.register(ToDoItemResource())
-
 urlpatterns = patterns('',
-	url(r'^$', include('todo.urls')),
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^api/', include(v1_api.urls)),
-	url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'todo/login.html'}, name='login'),
-	url(r'^logout/', 'django.contrib.auth.views.logout_then_login', name='logout'),
-	url(r'^signup/', views.signup, name='signup'),
+	url(r'^', include('todo.urls')),
 )
