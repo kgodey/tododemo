7a6162fb8a20ab7c8a99ed7ed566b20c4b6c3d6f
Kriti Godey <kriti@kritigodey.com>
2013-12-10 00:54:21 -0500

    signup works

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from todo.models import ToDoItem
 from tastypie.models import ApiKey
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 
 @login_required
 def index(request):
@@ -12,3 +14,15 @@ def index(request):
 	else:
 		context = {}
 	return render(request, 'todo/index.html', context)
+
+def signup(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			user = authenticate(username=request.POST['username'], password = request.POST['password1'])
+			login(request, user)
+			return redirect('/')
+	else:
+		form = UserCreationForm()
+	return render(request, 'todo/signup.html', {'form':form,})
--- toptaltodo/urls.py
+++ toptaltodo/urls.py
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
 	url(r'^api/', include(v1_api.urls)),
 	url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'todo/login.html'}),
 	url(r'^logout/', 'django.contrib.auth.views.logout_then_login'),
+	url(r'^signup/', views.signup),
 )
